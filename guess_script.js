a5b9c65dad3fa5be13856bb3d47569cfa7733ae3
tomas1978 <tomas.nilsson78@gmail.com>
2021-10-14 20:43:37 +0200

    Added check of guess is correct

--- guess_script.js
+++ guess_script.js
@@ -1,4 +1,22 @@
+var randomNumber=Math.random()*100;
+
 function processGuess() {
     var valueString=document.getElementById("guessInput").value;
+    var guess=parseInt(valueString);
+    var messageRef=document.getElementById("message");
+    messageRef.innerHTML="Du har gissat";
+    if(guess<randomNumber) {
+        messageRef.innerHTML="Din gissning är för låg";
+    }
+    else if(guess>randomNumber) {
+        messageRef.innerHTML="Din gissing är för hög";
+    }
+    else {
+        messageRef.innerHTML="Bra gjort! Du gissade rätt tal!";
+    }
+
     console.log(valueString);
+    
+    randomNumber=parseInt(randomNumber);
+    console.log(randomNumber);
 }
