009298d4c5f8b99b6fa691fff5113828ba3ffa5c
tomas1978 <tomas.nilsson78@gmail.com>
2021-10-15 16:44:13 +0200

    Introduced guess counter

--- guess_script.js
+++ guess_script.js
@@ -1,9 +1,13 @@
 var randomNumber=Math.random()*100;
+var counter=0;
 
 function processGuess() {
+	counter++;
     var valueString=document.getElementById("guessInput").value;
     var guess=parseInt(valueString);
     var messageRef=document.getElementById("message");
+	var guessesRef=document.getElementById("number_guesses);
+	guessesRef.innerHTML="Antal gissningar: "+counter;
     messageRef.innerHTML="Du har gissat";
     if(guess<randomNumber) {
         messageRef.innerHTML="Din gissning är för låg";
